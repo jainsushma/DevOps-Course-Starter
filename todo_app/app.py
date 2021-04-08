2dc7d7b7800a9d306181b25370b1bb1b51a6150a
jainsushma <sushma.crawley@gmail.com>
2021-04-08 18:55:05 +0000

    Added Get & Post methods

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import todo_app.data.session_items
 
 from todo_app.flask_config import Config
 
@@ -6,10 +7,17 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+@app.route('/items')
+def get_items():
+    items = todo_app.data.session_items.get_items()
+    return render_template("index.html", items=items)
 
+@app.route('/item', methods=['POST'])
+def add_item():
+    title = request.form['title']
+    todo_app.data.session_items.add_item(title)
+    items = todo_app.data.session_items.get_items()
+    return render_template("index.html", items=items)
 
 if __name__ == '__main__':
     app.run()
