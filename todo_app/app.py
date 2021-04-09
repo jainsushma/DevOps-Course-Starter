9458c6c9f0dfbb8b6ff8fa6b985b8f3f993a1b40
jainsushma <sushma.crawley@gmail.com>
2021-04-09 17:04:06 +0000

    Testing redirect

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import todo_app.data.session_items
 
 from todo_app.flask_config import Config
@@ -7,17 +7,20 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/items')
+@app.route('/')
 def get_items():
     items = todo_app.data.session_items.get_items()
     return render_template("index.html", items=items)
 
-@app.route('/item', methods=['POST'])
-def add_item():
+@app.route('/add_item', methods=['POST'])
+def add_new_item():
     title = request.form['title']
     todo_app.data.session_items.add_item(title)
-    items = todo_app.data.session_items.get_items()
-    return render_template("index.html", items=items)
+    # return redirect(f"/")
+    # todo_app.data.session_items.add_item(title)
+    # items = todo_app.data.session_items.get_items()
+    # return render_template("index.html", items=items)
+    return redirect(f"/")
 
 if __name__ == '__main__':
     app.run()
