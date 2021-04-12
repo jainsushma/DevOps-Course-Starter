345720a3da0b873c45f1cbe86fa15d562599d2e0
jainsushma <sushma.crawley@gmail.com>
2021-04-12 16:22:57 +0000

    add environment variables for redirecting to gitpod url

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import todo_app.data.session_items
+import os
 
 from todo_app.flask_config import Config
 
@@ -12,15 +13,19 @@ def get_items():
     items = todo_app.data.session_items.get_items()
     return render_template("index.html", items=items)
 
-@app.route('/add_item', methods=['POST'])
+@app.route('/add_item', methods=['POST', 'GET'])
 def add_new_item():
     title = request.form['title']
-    todo_app.data.session_items.add_item(title)
+    if request.method == 'POST':
+        title = request.form.get("title")
+        todo_app.data.session_items.add_item(title)
+        return redirect(os.getenv("HOSTNAME_GITPOD"))
+    # todo_app.data.session_items.add_item(title)
     # return redirect(f"/")
     # todo_app.data.session_items.add_item(title)
     # items = todo_app.data.session_items.get_items()
     # return render_template("index.html", items=items)
-    return redirect(f"/")
+    # return redirect("/")
 
 if __name__ == '__main__':
     app.run()
