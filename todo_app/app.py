f9346cfc112143a787c885d9afb3c1f670b88378
jainsushma <sushma.crawley@gmail.com>
2021-05-06 10:00:30 +0000

    adding code for moving card between the lists

--- todo_app/app.py
+++ todo_app/app.py
@@ -19,9 +19,15 @@ def get_items():
 @app.route('/add_item', methods=['POST'])
 def add_new_item():
     title = request.form['title']
-    session_items.add_item(title)
+    Trello_Board_Actions().addNewCard(title)
+    return redirect(os.getenv("TODO_HOSTNAME"))
+
+@app.route('/complete_item/<id>', methods=['POST'])
+def complete_item(id):
+    Trello_Board_Actions().changeCardStatus(id)
     return redirect(os.getenv("TODO_HOSTNAME"))
    
 
 if __name__ == '__main__':
-    app.run(key=os.getenv("SECRET_KEY"),token=os.getenv("TOKEN"))
+    # app.run(key=os.getenv("SECRET_KEY"),token=os.getenv("TOKEN"))
+    app.run()
--- todo_app/trello_board_actions.py
+++ todo_app/trello_board_actions.py
@@ -24,6 +24,68 @@ class Trello_Board_Actions:
         items = []
         for list in board_lists:
             for card in list["cards"]:
-                items.append({"title":card["name"], "status":card["closed"]})
+                items.append({"id":card['id'],"title":card['name'], "status":list['name']})
         return items
+
+    def addNewCard(self, title):
+        "Add a new card on the board"
+        result_flag = False
+        items = Trello_Board_Actions.getBoardLists(self)
+        self.payload = self.auth.copy()
+        self.payload['name'] = title
+        self.payload['idList'] = items[0]["cards"][0]["idList"]
+        add_new_card_url = self.url + "/cards"
+
+        response = requests.post(url=add_new_card_url,data=self.payload)
+        if response.status_code == 200:
+            result_flag = True
+
+        return result_flag
+    
+    def getCardIdByName(self,card_name):
+        "Get the card id"
+        card_id = None
+        card_url = self.url + '/boards/' + os.getenv('BOARD_ID') +'/cards'
+        card_details = requests.get(url=card_url,params=self.auth)
+        card_details = card_details.json()
+        for i in range(len(card_details)):
+            if card_details[i]['name'] == card_name:
+                card_id = card_details[i]['id']
+        return card_id 
+
+    def getListIdByName(self,list_name):
+        "Get the list id"
+        lists_on_board = Trello_Board_Actions.getBoardLists(self)
+        for i in range(len(lists_on_board)):
+            if lists_on_board[i]['name'] == list_name:
+                list_id = lists_on_board[i]['id']
+        return list_id 
+
+    def isCardOnList(self, list_name, card_id):
+        list_id = Trello_Board_Actions.getListIdByName(self, list_name)
+        list_url = self.url + '/boards/' + os.getenv('BOARD_ID') +'/'+ list_id + '/cards'
+        cards_on_the_list = requests.get(url=list_url,params=self.auth)
+        for i in range(len(cards_on_the_list)):
+            if cards_on_the_list[i]['id'] == card_id:
+                return True
+        return False
+
+    def changeCardStatus(self, id):
+        "Change card status"
+        result_flag = False
+        new_list_id = Trello_Board_Actions.getListIdByName(self, "done")
+        update_list_url = self.url + '/cards' + id
+        self.payload = self.auth.copy()
+        self.payload['idList'] = new_list_id
+        if Trello_Board_Actions.isCardOnList(self, "doing", id):
+            response = requests.put(url=update_list_url,data=self.payload)
+            if response.status_code == 200:
+                result_flag = True
+
+        return result_flag
+
+        
+    
+
+    
    
