7541383a9bb66dd459d2e2a0564f90d510b577b3
jainsushma <sushma.crawley@gmail.com>
2021-05-21 16:43:05 +0000

    removing result_flag

--- todo_app/trello_board_actions.py
+++ todo_app/trello_board_actions.py
@@ -36,14 +36,13 @@ class TrelloBoardActions:
 
     def add_new_card(self, title):
         "Add a new card on the board"
-        result_flag = False
         self.payload = self.auth.copy()
         self.payload['name'] = title
         self.payload['idList'] = self.get_list_id_by_name("to-do")
         response = requests.post(url=self.url + "/cards", data=self.payload)
         if response.status_code == 200:
-            result_flag = True
-        return result_flag
+            return True
+        return False
 
     def get_list_id_by_name(self,list_name):
         "Get the list id"
@@ -64,15 +63,13 @@ class TrelloBoardActions:
 
     def delete_card(self, card_id):
         "Delete any card"
-        result_flag = False
         response = requests.delete(url=self.url + '/cards/' + card_id, params=self.auth)
         if response.status_code == 200:
-            result_flag = True
-        return result_flag
+            return True
+        return False
 
     def update_card_status(self, id):
         "Update card status"
-        result_flag = False
         new_list_id = 0
         if self.is_card_on_list("to-do", id):
             new_list_id = self.get_list_id_by_name("doing")
@@ -85,9 +82,8 @@ class TrelloBoardActions:
         self.payload['idList'] = new_list_id
         response = requests.put(url=self.url + '/cards/' + id, data=self.payload)
         if response.status_code == 200:
-            result_flag = True
-
-        return result_flag
+            return True
+        return False
 
         
     
