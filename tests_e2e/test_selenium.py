30bf0dfa4131a2c645db5a86d1b937f1d159e89f
sjain309 <sjain309@aexp.com>
2021-06-05 00:03:30 +0100

    Add test journey

--- tests_e2e/test_selenium.py
+++ tests_e2e/test_selenium.py
@@ -52,10 +52,49 @@ def test_task_journey(driver, app_with_temp_board):
     driver.get('http://localhost:5000/')
     assert driver.title == 'To-Do App'
 
-      # Create a task
+    # Create a task
     driver.find_element_by_id("title").send_keys("New Item")
     # driver.find_element_by_xpath("//a[contains(text(),'Add Item')]").click()
     driver.find_element_by_id("addItem").click()
-    # time.sleep(3)
-    print("Tested")
+    time.sleep(5)
+    # verify if the task is in To Do list
+    value = driver.find_element_by_id("To-Do-Items")
+    time.sleep(2)
+    assert "New Item" in value.text
+    
+    # Move task to Doing list
+    driver.find_element_by_xpath("//a[contains(text(),'Start')]").click()
+    time.sleep(5)
+
+    # Verify if the task is in Doing list
+    value = driver.find_element_by_id("Doing-Items")
+    time.sleep(2)
+    assert "New Item" in value.text
+
+    # Move task to Done list
+    driver.find_element_by_xpath("//a[contains(text(),'Complete')]").click()
+    time.sleep(5)
+
+    # Verify if the task is in Done list
+    value = driver.find_element_by_id("Done-Items")
+    time.sleep(2)
+    assert "New Item" in value.text
+
+    # Move task to To Do list again
+    driver.find_element_by_xpath("//a[contains(text(),'Mark as Incomplete')]").click()
+    time.sleep(5)
+
+    # Verify if the task is in To Do list
+    value = driver.find_element_by_id("To-Do-Items")
+    time.sleep(2)
+    assert "New Item" in value.text
+
+    # Delete the task
+    driver.find_element_by_xpath("//a[contains(text(),'Delete')]").click()
+    time.sleep(5)
+
+    # Verify if the task is deleted
+    value = driver.find_element_by_id("To-Do-Items")
+    time.sleep(2)
+    assert "New Item" not in value.text
     # driver.quit()
