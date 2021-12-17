0431b871a923bb54b2abc3b5b26eb2c7ce79e78a
Sushma Jain <jainsushma@Sushmas-Air.broadband>
2021-12-17 12:13:30 +0000

    Adding chromedriver to the path

--- tests_e2e/test_selenium.py
+++ tests_e2e/test_selenium.py
@@ -5,6 +5,7 @@ import time
 from threading import Thread
 from dotenv import find_dotenv, load_dotenv
 import todo_app.app as app
+from todo_app.mongo_actions import MongoActions
 from selenium import webdriver
 
 @pytest.fixture(scope="module")
@@ -33,7 +34,8 @@ def app_with_temp_board():
     # Tear Down
     thread.join(1)
     time.sleep(3)
-    
+    mongoActions = MongoActions()
+    mongoActions.db.drop()
 
 def test_task_journey(driver, app_with_temp_board):
     driver.implicitly_wait(5)
