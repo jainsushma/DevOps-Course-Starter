011830c67504f324dd7030696a540e79d14ed66d
Sushma Jain <jainsushma@Sushmas-Air.broadband>
2022-04-06 17:49:39 +0100

    Adding logs into loggly

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -4,5 +4,7 @@ class Config:
     """Base configuration variables."""
     SECRET_KEY = os.getenv('SECRET_KEY')
     LOGIN_DISABLED = os.getenv('LOGIN_DISABLED')=="True"
+    LOG_LEVEL = os.getenv('LOG_LEVEL')
+    LOGGLY_TOKEN = os.getenv('LOGGLY_TOKEN')
     if not SECRET_KEY:
         raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
